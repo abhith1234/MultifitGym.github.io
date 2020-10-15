b47b4e06616e018d849c59123461f8433e53d4e5
abhishek thombare <65451040+abhith1234@users.noreply.github.com>
2020-10-15 16:03:19 +0530

    Add files via upload

---
+++ js2.js
@@ -0,0 +1,76 @@
+var shadowitem1=document.getElementById("item1");
+var shadowitem2=document.getElementById("item2");
+var shadowitem3=document.getElementById("item3");
+var shadowitem4=document.getElementById("item4");
+var shadowitem5=document.getElementById("item5");
+var shadowitem6=document.getElementById("item6");
+var shadowitem7=document.getElementById("item7");
+
+shadowitem1.addEventListener('mouseover',function(){
+	shadowitem1.classList.remove('shadow');
+	shadowitem1.classList.add('shadow-lg');
+});
+shadowitem1.addEventListener('mouseout',function(){
+	shadowitem1.classList.remove('shadow-lg');
+	shadowitem1.classList.add('shadow');
+});
+
+shadowitem2.addEventListener('mouseover',function(){
+	shadowitem2.classList.remove('shadow');
+	shadowitem2.classList.add('shadow-lg');
+
+});
+shadowitem2.addEventListener('mouseout',function(){
+	shadowitem2.classList.remove('shadow-lg');
+	shadowitem2.classList.add('shadow');
+});
+
+shadowitem3.addEventListener('mouseover',function(){
+	shadowitem3.classList.remove('shadow');
+	shadowitem3.classList.add('shadow-lg');
+
+});
+shadowitem3.addEventListener('mouseout',function(){
+	shadowitem3.classList.remove('shadow-lg');
+	shadowitem3.classList.add('shadow');
+});
+
+shadowitem4.addEventListener('mouseover',function(){
+	shadowitem4.classList.remove('shadow');
+	shadowitem4.classList.add('shadow-lg');
+
+});
+shadowitem4.addEventListener('mouseout',function(){
+	shadowitem4.classList.remove('shadow-lg');
+	shadowitem4.classList.add('shadow');
+});
+
+shadowitem5.addEventListener('mouseover',function(){
+	shadowitem5.classList.remove('shadow');
+	shadowitem5.classList.add('shadow-lg');
+
+});
+shadowitem5.addEventListener('mouseout',function(){
+	shadowitem5.classList.remove('shadow-lg');
+	shadowitem5.classList.add('shadow');
+});
+
+shadowitem6.addEventListener('mouseover',function(){
+	shadowitem6.classList.remove('shadow');
+	shadowitem6.classList.add('shadow-lg');
+
+});
+shadowitem6.addEventListener('mouseout',function(){
+	shadowitem6.classList.remove('shadow-lg');
+	shadowitem6.classList.add('shadow');
+});
+
+shadowitem7.addEventListener('mouseover',function(){
+	shadowitem7.classList.remove('shadow');
+	shadowitem7.classList.add('shadow-lg');
+
+});
+shadowitem7.addEventListener('mouseout',function(){
+	shadowitem7.classList.remove('shadow-lg');
+	shadowitem7.classList.add('shadow');
+});
---
+++ js3.js
@@ -0,0 +1,209 @@
+
+function hiit()
+{
+	var divpg=document.getElementById("info");
+	var imag=document.createElement('img');
+	var brk=document.createElement('br');
+	var nele=document.createElement('div');
+	var title=document.createElement('h2');
+	title.appendChild(document.createTextNode("HIIT Training"));
+	imag.src="./hiit.png"
+	nele.appendChild(title);
+	nele.appendChild(imag);
+	nele.appendChild(brk);
+	nele.appendChild(document.createTextNode("High Intensity Interval Training with variations in workouts focuses on different aspects of fitness i.e. strength, stamina, endurance, and agility. Each class is designed based on the format that is scientifically created by a panel of experts, with the potential to torch maximum calories in a minimum amount of time. HIIT is all about doing short periods of all-out work followed by short periods of active rest. This makes the body work harder than it does during steady-state cardio. Out trainers customize each workout routine making it fun and motivating and correct the Forms and techniques in six different classes designed specifically through the week."));
+
+	divpg.innerHTML=""
+	divpg.appendChild(nele);
+
+}
+
+function functional()
+{
+	var divpg=document.getElementById("info");
+	var imag=document.createElement('img');
+	var brk=document.createElement('br');
+	var nele=document.createElement('div');
+	var title=document.createElement('h2');
+	title.appendChild(document.createTextNode("Functional Training"));
+	imag.src="./functional.png"
+	nele.appendChild(title);
+	nele.appendChild(imag);
+	nele.appendChild(brk);
+	nele.appendChild(document.createTextNode("Functional training simply means training your body to perform everyday movements such as lifting bending, climbing stairs. Functional training helps in improving the functional movements. The time spent training with functional equipment in open spaces improves different fitness aspects by developing your agility, flexibility and dynamic strength. This training can be undertaken by anyone from a beginner to an athlete can start training at MultiFit."));
+
+	divpg.innerHTML=""
+	divpg.appendChild(nele);
+
+}
+
+function personal()
+{
+	var divpg=document.getElementById("info");
+	var imag=document.createElement('img');
+	var brk=document.createElement('br');
+	var nele=document.createElement('div');
+	var title=document.createElement('h2');
+	title.appendChild(document.createTextNode("Personal Training"));
+	imag.src="./personal.png"
+	nele.appendChild(title);
+	nele.appendChild(imag);
+	nele.appendChild(brk);
+	nele.appendChild(document.createTextNode("Personal training can be done by any individual who requires personal attention to move closer to their goals. A customized workout and 1:1 training with the trainer to help reach your individual goals that may include anything from weight loss, gaining strength, injury rehab, and conditioning for a sport. A personal trainer gives one a disciplined workout and helps one start out with the correct forms and techniques"));
+
+	divpg.innerHTML=""
+	divpg.appendChild(nele);
+
+}
+
+function MMa()
+{
+	var divpg=document.getElementById("info");
+	var imag=document.createElement('img');
+	var brk=document.createElement('br');
+	var nele=document.createElement('div');
+	var title=document.createElement('h2');
+	title.appendChild(document.createTextNode("MMA"));
+	imag.src="./mma.png"
+	nele.appendChild(title);
+	nele.appendChild(imag);
+	nele.appendChild(brk);
+	nele.appendChild(document.createTextNode("MMA (Mix Martial Arts) is a full body workout that pushes your body to the limit like no other sport while working your upper body, lower body and core. It’s also a high intensity workout that challenges your cardiovascular muscles and burns calories quickly. You can burn up to 590 calories in a single 45-minute session. Overall, MMA workouts incorporate strength, aerobic and metabolic conditioning, so you gain endurance, strength, agility, and power. MMA comprises kick-boxing, grappling, jui-jitsu, judo, and much more."));
+
+	divpg.innerHTML=""
+	divpg.appendChild(nele);
+
+}
+
+function yoga()
+{
+	var divpg=document.getElementById("info");
+	var imag=document.createElement('img');
+	var brk=document.createElement('br');
+	var nele=document.createElement('div');
+	var title=document.createElement('h2');
+	title.appendChild(document.createTextNode("Yoga"));
+	imag.src="./yoga.png"
+	nele.appendChild(title);
+	nele.appendChild(imag);
+	nele.appendChild(brk);
+	nele.appendChild(document.createTextNode("Yoga focuses on three major factors in fitness; our mind, body and soul. Yoga helps increase our bodies strength, core stability & flexibility whilst helping improve our heart & circulatory health. At Multifit we focus on each individuals needs to help increase your body both physically and mentally. We look at all aspects of Yoga for clients to ensure we improve each and everyone that enters our studio doors."));
+
+	divpg.innerHTML=""
+	divpg.appendChild(nele);
+
+}
+
+function boxing()
+{
+	var divpg=document.getElementById("info");
+	var imag=document.createElement('img');
+	var brk=document.createElement('br');
+	var nele=document.createElement('div');
+	var title=document.createElement('h2');
+	title.appendChild(document.createTextNode("Boxing"));
+	imag.src="./boxing.png"
+	nele.appendChild(title);
+	nele.appendChild(imag);
+	nele.appendChild(brk);
+	nele.appendChild(document.createTextNode("Boxing is one of the best forms of cardio in addition to improving hand and eye coordination, decreases stress. Boxing can be done by all age groups and offers a form of self-defence. At Multifit, you can practise in a boxing ring for a great experience while working out."));
+
+	divpg.innerHTML=""
+	divpg.appendChild(nele);
+
+}
+
+function calis()
+{
+	var divpg=document.getElementById("info");
+	var imag=document.createElement('img');
+	var brk=document.createElement('br');
+	var nele=document.createElement('div');
+	var title=document.createElement('h2');
+	title.appendChild(document.createTextNode("Calisthenics"));
+	imag.src="./Calisthenics.png"
+	nele.appendChild(title);
+	nele.appendChild(imag);
+	nele.appendChild(brk);
+	nele.appendChild(document.createTextNode("Calisthenics is a form of physical exercise, which utilises one’s own body weight for strength training. Herein, through bodyweight training we focus on strengthening the core. The workout involves complex movements while training different muscle groups. The workout includes exercises like human flags, push ups, pull ups, and muscle ups. Individuals of all age groups or beginners can start training."));
+
+	divpg.innerHTML=""
+	divpg.appendChild(nele);
+
+}
+
+function strength()
+{
+	var divpg=document.getElementById("info");
+	var imag=document.createElement('img');
+	var brk=document.createElement('br');
+	var nele=document.createElement('div');
+	var title=document.createElement('h2');
+	title.appendChild(document.createTextNode("strength and conditioning"));
+	imag.src="./strength.png"
+	nele.appendChild(title);
+	nele.appendChild(imag);
+	nele.appendChild(brk);
+	nele.appendChild(document.createTextNode("Strength and Conditioning is the physical and physiological development of athletes elite sports performance. At Multifit we work with a number of athletes across various sports to help improve their performance within chosen sport. S&C focuses on more than just lifting weights; we look at speed, agility, co-ordination, plyometrics, strength, power, core stability & conditioning of the body to ensure that they are at the top of their game all year round."));
+
+	divpg.innerHTML=""
+	divpg.appendChild(nele);
+
+}
+
+function looping()
+{
+	var divpg=document.getElementById("info");
+	var imag=document.createElement('img');
+	var brk=document.createElement('br');
+	var nele=document.createElement('div');
+	var title=document.createElement('h2');
+	title.appendChild(document.createTextNode("Looping"));
+	imag.src="./looping.png"
+	nele.appendChild(title);
+	nele.appendChild(imag);
+	nele.appendChild(brk);
+	nele.appendChild(document.createTextNode("Looping is a high intensity indoor cycling that specifically targets in boosting your cardio fitness. In looping, you spin the pedals repeatedly to reach the cardio peak and then ease back down. This full body workout is a great way to lift your personal performance in the group, as you draw on the group’s energy, looping to the rhythm of music. The instructor driven strategy and the music helps you monitor resistance levels and speed. The instructor motivates the group to help improving the cardio and muscle capacity."));
+
+	divpg.innerHTML=""
+	divpg.appendChild(nele);
+
+}
+
+function weight()
+{
+	var divpg=document.getElementById("info");
+	var imag=document.createElement('img');
+	var brk=document.createElement('br');
+	var nele=document.createElement('div');
+	var title=document.createElement('h2');
+	title.appendChild(document.createTextNode("weight Training"));
+	
+	nele.appendChild(title);
+	
+	nele.appendChild(brk);
+	nele.appendChild(document.createTextNode("The term ‘free weights’ is a training style focusing on only weight training helping an individual improve their strength and size of skeletal muscle. The force of what one can push or pull with various bodies parts using only weighted bars, dumbbells, weight plates or weight stacks through concentric or eccentric contraction during an exercise. At Multifit we ensure that you will learn the correct form during exercises when carrying out weight training with our experienced international coaches"));
+
+	divpg.innerHTML=""
+	divpg.appendChild(nele);
+
+}
+
+function trans()
+{
+	var divpg=document.getElementById("info");
+	var imag=document.createElement('img');
+	var brk=document.createElement('br');
+	var nele=document.createElement('div');
+	var title=document.createElement('h2');
+	title.appendChild(document.createTextNode("Transformation"));
+	imag.src="./trans.png"
+	nele.appendChild(title);
+	nele.appendChild(imag);
+	nele.appendChild(brk);
+	nele.appendChild(document.createTextNode("• Get custom workout and nutrition plans based on YOUR goal.We are a dedicated team of certified trainers and sports nutritionists determined to give you the healthy and fit body youve always dreamt of. Through a variety of tailor made and customised packages, we work with you on: weight loss, muscle strength, toning, improving metabolism, and achieving a healthy and fit life overall.So, whether youre a new mother trying to lose pregnancy weight, a busy corporate person dreaming to be healthy, or a professional athlete looking for precision inch loss, we can be your Transformation coaches!"));
+
+	divpg.innerHTML=""
+	divpg.appendChild(nele);
+
+}
